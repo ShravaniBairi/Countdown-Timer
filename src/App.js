3b697aebdb61e2818fca1c6f3f4c9523585b8014
ShravaniBairi <shravani286k@gmail.com>
2024-05-09 17:11:33 +0200

    added Stop feature

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import React, {useState, useEffect} from 'react';
 
 
 
+
 function App() {
 
   const [targetDatetime, setTargetDatetime] = useState()
   const [submittedDatetime, setSubmittedDatetime] = useState()
+  const [resultMessage, setResultMessage] = useState('')
+    const [resultTime, setResultTime] = useState({ resultTime: {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+    }})
   //const [timerStatus, setTimerStatus] = useState(false)
-  const [output, setOutput] = useState()
+  
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,53 +27,53 @@ function App() {
   }
  
 
+  
+  
+
   useEffect(()=>{
-   
-   
     if(submittedDatetime !== undefined) {
-
-      const updateTimer = () => {
-
+    const UpdateTimer = () => {
       const presentTime = new Date().getTime();
-      const target = new Date(submittedDatetime).getTime()
-      const countdownTime = target-presentTime
-      const days = Math.floor(countdownTime/(1000*60*60*24));
-
-      if(days > 99 ) {
-        return setOutput("select less than 99 days")
+      const targetTime = new Date(submittedDatetime).getTime();
+      const countdownTime = targetTime-presentTime;
+      const days = Math.floor(countdownTime / (1000*60*60*24))
+      if(countdownTime < 0 ) {
+          setResultMessage('Target Reached')
       }
-      else if(countdownTime <= 0) {
-        return setOutput("Reached Target");
-
+      else if (days > 99){
+          setResultMessage('Select time less than 100 Days')
       }
-      else{
-      
-
-        let total_seconds = Math.floor(countdownTime/1000);
-        let total_minutes = Math.floor(total_seconds/60);
-        let total_hours = Math.floor(total_minutes/60);
-        let total_days = Math.floor(total_hours/24);
-
-        let seconds = total_seconds%60
-        let minutes = total_minutes % 60
-        let hours = total_hours % 24
-
-        return setOutput(`${total_days}:${hours}:${minutes}:${seconds}`)
-
-
+      else 
+      {
+          const total_seconds= Math.floor(countdownTime / 1000)
+          const total_minutes = Math.floor(total_seconds/60)
+          const total_hours = Math.floor(total_minutes/60)
+  
+          const R_hours = total_hours%24
+          const R_minutes = total_minutes%60
+          const R_seconds = total_seconds%60 
+  
+          setResultTime({
+              days: days,
+              hours: R_hours,
+              minutes: R_minutes,
+              seconds: R_seconds
+          })
       }
     }
-    
-    const timerId = setInterval(updateTimer, 1000);
 
-    return () => clearInterval(timerId);
-   
+    const timerId = setInterval(()=>{UpdateTimer(submittedDatetime, setResultMessage, setResultTime)}, 1000)
+  
 
-    }
-    
-    
+    return () => clearInterval(timerId)    
+  }
 
   } ,[submittedDatetime])
+   
+
+    
+    
+    
   
 
   return (
@@ -84,9 +92,30 @@ function App() {
 
       </form>
 
+     
+
       
 
-      <div className= " text-center text-blue-900 font-bold sm:text-4xl text-3xl" >{output}</div>
+       <div className= " text-center text-blue-900 font-bold sm:text-4xl text-3xl" >{resultMessage} </div> 
+       <div className='flex flex-wrap  justify-center  xl:px-60 sm:px-10 sm:mx-10   px-4'>
+        <div className='m-4 font-bold  h-auto sm:px-16 sm:py-10 p-2   rounded-lg  shadow-lg  bg-gradient-to-bl  from-red-400  via-pink-200 to-red-300 '>
+            <h1 className=' text-center text-4xl'>{resultTime?.days}</h1>
+            <h1 className=' text-center text-xl'>DAYS</h1>
+        </div>
+        <div className='m-4 font-bold  h-auto sm:px-16 sm:py-10 p-2   rounded-lg  shadow-lg  bg-gradient-to-bl  from-red-400  via-pink-200 to-red-300 '>
+            <h1 className=' text-center text-4xl'>{resultTime?.hours}</h1>
+            <h1 className=' text-center text-xl'>HOURS</h1>
+        </div>
+        <div className='m-4 font-bold  h-auto sm:px-16 sm:py-10 p-2   rounded-lg  shadow-lg  bg-gradient-to-bl  from-red-400  via-pink-200 to-red-300 '>
+            <h1 className=' text-center text-4xl'>{resultTime?.minutes}</h1>
+            <h1 className=' text-center text-xl'>MINUTES</h1>
+        </div>
+        <div className='m-4 font-bold  h-auto sm:px-16 sm:py-10 p-2   rounded-lg  shadow-lg  bg-gradient-to-bl  from-red-400  via-pink-200 to-red-300 '>
+            <h1 className=' text-center text-4xl'>{resultTime?.seconds}</h1>
+            <h1 className=' text-center text-xl'>SECONDS</h1>
+        </div>
+    </div>
+
 
      
       
